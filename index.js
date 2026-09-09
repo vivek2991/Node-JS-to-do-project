32e5fe2afbc96ae6b5c81e6c2109a2e64c76a3e4
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-09 15:16:20 +0530

    #5 | Display Task List in Browser | To Do App with MongoDB

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ const connection = async () => {
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, resp) => {
-    resp.render("list")
+app.get('/', async (req, resp) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = await collection.find().toArray();
+    resp.render("list", {result})
 })
 
 app.get('/add', (req, resp) => {
