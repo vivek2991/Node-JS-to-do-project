f5a6083f7918a9a4abfcd1bc250d478a0aa33614
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-09 14:41:36 +0530

    #3 Make Add and Update Task Form

--- index.js
+++ index.js
@@ -19,4 +19,13 @@ app.get('/update', (req, resp)=>{
     resp.render("update")
 })
 
+app.post('/add', (req, resp)=>{
+    resp.redirect('/')
+})
+
+app.post('/update', (req, resp)=>{
+    resp.redirect('/')
+})
+
+
 app.listen(3200);
