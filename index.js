c890d0676590d026717af277ca1aa3de06597046
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-09 14:59:15 +0530

    #4 | Add Tasks in MongoDB Database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const publicPath = path.resolve('public')
@@ -7,23 +8,43 @@ const publicPath = path.resolve('public')
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs')
 
-app.get('/', (req, resp)=>{
+const dbName = "node-project"
+const collectionName = "todo"
+const url = "mongodb://localhost/27017"
+const client = new MongoClient(url)
+
+const connection = async () => {
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, resp) => {
     resp.render("list")
 })
 
-app.get('/add', (req, resp)=>{
+app.get('/add', (req, resp) => {
     resp.render("add")
 })
 
-app.get('/update', (req, resp)=>{
+app.get('/update', (req, resp) => {
     resp.render("update")
 })
 
-app.post('/add', (req, resp)=>{
-    resp.redirect('/')
+app.post('/add', async (req, resp) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = collection.insertOne(req.body)
+    if (result) {
+        resp.redirect('/')
+    } else {
+        resp.redirect('/add')
+    }
+
 })
 
-app.post('/update', (req, resp)=>{
+app.post('/update', (req, resp) => {
     resp.redirect('/')
 })
 
